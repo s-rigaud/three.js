f1c0d8c6f91ad378ca286a5528e72be450a12237
Samuel Rigaud <srigaud@duodisplay.com>
2025-03-10 08:45:44 -0400

    revert eslint ignore

--- src/core/Object3D.js
+++ src/core/Object3D.js
@@ -719,13 +719,10 @@ class Object3D extends EventDispatcher {
 	 */
 	add( object ) {
 
-		// eslint-disable-next-line
 		if ( arguments.length > 1 ) {
 
-			// eslint-disable-next-line
 			for ( let i = 0; i < arguments.length; i ++ ) {
 
-				// eslint-disable-next-line
 				this.add( arguments[ i ] );
 
 			}
@@ -774,13 +771,10 @@ class Object3D extends EventDispatcher {
 	 */
 	remove( object ) {
 
-		// eslint-disable-next-line
 		if ( arguments.length > 1 ) {
 
-			// eslint-disable-next-line
 			for ( let i = 0; i < arguments.length; i ++ ) {
 
-				// eslint-disable-next-line
 				this.remove( arguments[ i ] );
 
 			}
